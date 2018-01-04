6dc1d477234663547d0328acb7524d87e423906f
Administrator <Administrator@73Q4K31HY602G2V>
2018-01-04 22:17:50 +0800

    1

---
+++ src/gjhg/FF.java
@@ -0,0 +1,15 @@
+package gjhg;
+
+/**  
+* @ClassName: FF  
+* @Description: TODO
+* @author Administrator  
+* @date 2018年1月4日  下午10:14:22
+*  
+*/
+public class FF {
+
+	public static void main(String[] args) {
+		System.out.println(111);
+	}
+}
